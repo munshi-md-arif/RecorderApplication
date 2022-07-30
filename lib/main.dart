d11407fda978867b8b819885d458e52dea9e78f4
munshi-md-arif <arifmunshi278@gmail.com>
2022-07-30 13:56:36 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:test_audio/recorder_home_view.dart';
+// import 'package:voice_reocrder/views/recorder_home_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Voice Recorder',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RecorderHomeView(
+        title: 'Flutter Voice',
+      ),
+    );
+  }
+}
